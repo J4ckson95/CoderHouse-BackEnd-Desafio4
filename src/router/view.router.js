9878ad32627875b3ce92965d35b47408a5533829
Jason López <j4ckson95@gmail.com>
2023-10-31 22:26:40 -0500

    Adelanto

--- src/router/view.router.js
+++ src/router/view.router.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import fs from "fs"
 
 const router = Router()
+const path = "./src/data/DB.json"
 
 router.get("/", (req, res) => {
-    res.render("index", {})
+    fs.promises.readFile(path, "utf-8")
+        .then((data) => {
+            const dataFromFile = JSON.parse(data)
+            res.render("index", { dataFromFile })
+        })
 })
 
 export default router
