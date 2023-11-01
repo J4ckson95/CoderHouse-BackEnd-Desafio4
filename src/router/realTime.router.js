6ff4aab11ed7f5bdcb95aa2b240152f9273b00bb
Jason <j4ckson95@gmail.com>
2023-11-01 14:37:57 -0500

    Adelanto oficina

--- src/router/realTime.router.js
+++ src/router/realTime.router.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import fs from "fs"
 
 const router = Router()
+const path = "./src/data/DB.json"
 
 router.get("/", (req, res) => {
-    res.render("realTimeProducts", {})
+    fs.promises.readFile(path, "utf-8")
+        .then(data => {
+            const dataFromFile = JSON.parse(data)
+            res.render("realTimeProducts", { dataFromFile })
+        })
 })
 
 export default router
